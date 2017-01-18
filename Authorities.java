5a09ffe3dd02a29978f5c574af12f3b84b0d983a
shubham-varshney <shubhamvarshney16@gmail.com>
2017-01-18 21:51:49 +0530

    login page,user.java, authority.java, home controller
    done with changes in header file. completed authority and user class. done with spring security

---
+++ Authorities.java
@@ -0,0 +1,45 @@
+package com.niit.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Authorities {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int authorisedId;
+	
+	private String username;
+	
+	private String authority;
+
+	public int getAuthorisedId() {
+		return authorisedId;
+	}
+
+	public void setAuthorisedId(int authorisedId) {
+		this.authorisedId = authorisedId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+	
+	
+	
+}
